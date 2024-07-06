891c4c4bc8f6c684e9b651586b1a5768dd56bc4f
rkran <rkranjith03@gmail.com>
2024-07-06 10:27:08 +0530

    bugs

--- main.py
+++ main.py
@@ -13,28 +13,32 @@ def home():
     result = predict_failure('copy_Problem_Statement_2_ Data_set.xlsx')
     result = result.where(pd.notnull(result), None)
     prediction=[]
-    for item in result.to_dict(orient='records')[0:2]:
-        if item['target']=='Safe':
-            prompt=f"Give me recommendations to maintain the {item["param_mapping_x"]} in my {item["Machine"]}."
+    for item in result.to_dict(orient='records')[0:10]:
+        if item['target']=='Safe' and item['Probability of Failure']=='High':
+            continue
         else:
-            if not item["high thres"]:
-                prompt=f"Let's say {item["param_mapping_x"]} of a {item["Machine"]} is {item["Value_x"]} and it has a probability of failure if it goes below {item["low thres"]}. Give recommendations to improve it."
-            elif not item["low thres"]:
-                prompt = f"Let's say {item["param_mapping_x"]} of a {item["Machine"]} is {item["Value_x"]} and it has a probability of failure if it exceeds below {item["high thres"]}. Give recommendations to improve it."
+            if item['target'] == 'Safe':
+                prompt = f"Give me recommendations to maintain the {item["param_mapping_x"]} in my {item["Machine"]}."
             else:
-                prompt = f"Let's say {item["param_mapping_x"]} of a {item["Machine"]} is {item["Value_x"]} and it has a probability of failure if it goes below {item["low thres"]} and exceeds {item["high thres"]}. Give recommendations to improve it."
-        response = ollama.chat(model='llama3', messages=[
-            {
-                'role': 'user',
-                'content': prompt,
-            },
-        ])
-        del item['low thres']
-        del item['high thres']
-        item['suggestion']=response['message']['content']
-        item['Time']=item['Time'].strftime("%Y-%m-%d %H:%M:%S")
-        print(response)
-        prediction.append(item)
+                if np.isnan(item["high thres"]):
+                    prompt = f"Let's say {item["param_mapping_x"]} of a {item["Machine"]} is {item["Value_x"]} and it has a probability of failure if it goes below {item["low thres"]}. Give recommendations to improve it."
+                elif np.isnan(item["low thres"]):
+                    prompt = f"Let's say {item["param_mapping_x"]} of a {item["Machine"]} is {item["Value_x"]} and it has a probability of failure if it exceeds below {item["high thres"]}. Give recommendations to improve it."
+                else:
+                    prompt = f"Let's say {item["param_mapping_x"]} of a {item["Machine"]} is {item["Value_x"]} and it has a probability of failure if it goes below {item["low thres"]} and exceeds {item["high thres"]}. Give recommendations to improve it."
+            response = ollama.chat(model='llama3', messages=[
+                {
+                    'role': 'user',
+                    'content': prompt,
+                },
+            ])
+            del item['low thres']
+            del item['high thres']
+            item['suggestion'] = response['message']['content']
+            item['Time'] = item['Time'].strftime("%Y-%m-%d %H:%M:%S")
+            print(response)
+            prediction.append(item)
+
     return jsonify(prediction)
 
 
